3210a088e34e076694edb38d33402b05149cd4f0
cesar214567 <cesar.madera@utec.edu.pe>
2019-09-24 22:31:25 -0500

    pretty sure is done

--- web/server.py
+++ web/server.py
@@ -475,4 +475,4 @@ def signin():
 
 if __name__ == '__main__':
     app.secret_key = ".."
-    app.run(port=8080, threaded=True, host=('127.0.0.1'))
+    app.run(port=8080, threaded=True, host=('http://munana.herokuapp.com'))
